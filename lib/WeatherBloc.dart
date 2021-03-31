7391a3653f6b5b3cf1a309bc01152a8781f62d4d
astisk <pobierac@gmail.com>
2021-03-31 21:51:10 +0200

    First commit, main app

--- lib/WeatherBloc.dart
+++ lib/WeatherBloc.dart
@@ -5,9 +5,7 @@ import 'package:flutter_app/WeatherRepo.dart';
 
 class WeatherEvent extends Equatable{
   @override
-  // TODO: implement props
   List<Object> get props => [];
-
 }
 
 class FetchWeather extends WeatherEvent{
@@ -16,29 +14,19 @@ class FetchWeather extends WeatherEvent{
   FetchWeather(this._city);
 
   @override
-  // TODO: implement props
   List<Object> get props => [_city];
 }
 
-class ResetWeather extends WeatherEvent{
-
-}
+class ResetWeather extends WeatherEvent{}
 
 class WeatherState extends Equatable{
   @override
-  // TODO: implement props
   List<Object> get props => [];
-
 }
 
+class WeatherIsNotSearched extends WeatherState{}
 
-class WeatherIsNotSearched extends WeatherState{
-
-}
-
-class WeatherIsLoading extends WeatherState{
-
-}
+class WeatherIsLoading extends WeatherState{}
 
 class WeatherIsLoaded extends WeatherState{
   final _weather;
@@ -48,32 +36,20 @@ class WeatherIsLoaded extends WeatherState{
   WeatherModel get getWeather => _weather;
 
   @override
-  // TODO: implement props
   List<Object> get props => [_weather];
 }
 
-class WeatherIsNotLoaded extends WeatherState{
-
-}
+class WeatherIsNotLoaded extends WeatherState{ }
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState>{
 
   WeatherRepo weatherRepo;
 
-  //WeatherBloc(this.weatherRepo);
-
   WeatherBloc(WeatherState initialState, this.weatherRepo) : super(initialState);
-  //
-  // @override
-  // // TODO: implement initialState
-  // WeatherState get initialState => WeatherIsNotSearched();
-
   @override
   Stream<WeatherState> mapEventToState(WeatherEvent event) async*{
-    // TODO: implement mapEventToState
     if(event is FetchWeather){
       yield WeatherIsLoading();
-
       try{
         WeatherModel weather = await weatherRepo.getWeather(event._city);
         yield WeatherIsLoaded(weather);
@@ -85,5 +61,4 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState>{
       yield WeatherIsNotSearched();
     }
   }
-
 }
